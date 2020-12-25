3d2d54075a28663782d6303ed90219f79a206bdb
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2020-12-25 13:54:22 -0300

    Add files via upload

--- ps4.js
+++ ps4.js
@@ -411,7 +411,7 @@ function prepareUAF() {
 
 /* HTMLElement spray */
 function sprayHTMLTextArea() {
-	debug_log("[+] Spraying HTMLTextareaElement ...");
+	debug_log("[+] Pulverizando elementos de área em HTML ...");
 
 	let textarea_div_elem = document.createElement("div");
 	document.body.appendChild(textarea_div_elem);
